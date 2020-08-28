960e5b19b89c7483e3cf476b06a4019503ab68ba
Asif Khan <asifkhan.github@gmail.com>
2020-08-29 00:04:42 +0600

    method created for showing all the articles

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -8,9 +8,12 @@ use Validator;
 use Response;
 use App\Http\Resources\ArticleResource;
 use Illuminate\Validation\Rule;
-use App\Model\Article;
+use App\Models\Article;
 use Auth;
 
 class ArticleController extends Controller {
-    
+    // show all the article
+    public function index(){
+        return ArticleResource::collection(Article::orderBy('id','DESC')->paginate(10));
+    }
 }
