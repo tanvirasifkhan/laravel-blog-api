778669036651e95eb0e33b7e19c1e452ceedb2a4
Asif Khan <asifkhan.github@gmail.com>
2020-08-29 03:22:15 +0600

    id column added for author resource

--- app/Http/Resources/AuthorResource.php
+++ app/Http/Resources/AuthorResource.php
@@ -15,6 +15,7 @@ class AuthorResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'=>$this->id,
             'name'=>$this->name,
             'email'=>$this->email
         ];
