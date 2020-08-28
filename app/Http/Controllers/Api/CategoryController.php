a50d0c99f0505dd1964825e6679a9ca52323b60b
Asif Khan <asifkhan.github@gmail.com>
2020-08-28 23:23:01 +0600

    method created for storing category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -15,4 +15,21 @@ class CategoryController extends Controller {
     public function index(){
         return CategoryResource::collection(Category::orderBy('id','DESC')->paginate(10));
     }
+
+    // store new category into the database
+    public function store(Request $request){
+        $validators=Validator::make($request->all(),[
+            'title'=>'required|unique:categories',
+            'slug'=>'required|unique:categories'
+        ]);
+        if($validators->fails()){
+            return Response::json(['errors'=>$validators->getMessageBag()->toArray()]);
+        }else{
+            $category=new Category();
+            $category->title=$request->title;
+            $category->slug=strtolower(implode('-',explode(' ',$request->slug)));
+            $category->save();
+            return Response::json(['success'=>'Category created successfully !']);
+        }
+    }
 }
