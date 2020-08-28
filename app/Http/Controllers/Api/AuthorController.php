02b729d91b20e1afaa9ee052bd751aff404db85f
Asif Khan <asifkhan.github@gmail.com>
2020-08-29 03:22:46 +0600

    method created for showing authors

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\AuthorResource;
+use App\Models\User;
+use Response;
+use Validator;
+use Illuminate\Support\Str;
+use Auth;
 
 class AuthorController extends Controller {
-    
+    // show all the users
+    public function index(){
+        return AuthorResource::collection(User::orderBy('id','DESC')->paginate(10));
+    }
 }
