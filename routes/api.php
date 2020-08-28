12893f3198cca7b2d672b2ff182e55d60cae4259
Asif Khan <asifkhan.github@gmail.com>
2020-08-28 23:11:39 +0600

    route created for showing all the categories

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**********************************   Category Route Starts Here   *******************************************/
+Route::get('categories','CategoryController@index');
+/**********************************   Category Route Ends Here   *******************************************/
