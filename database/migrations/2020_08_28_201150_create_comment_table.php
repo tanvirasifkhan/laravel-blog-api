e6dd866fe8218e403d64331514c23497329d8f0a
Asif Khan <asifkhan.github@gmail.com>
2020-08-29 02:40:42 +0600

    migration created for comment

---
+++ database/migrations/2020_08_28_201150_create_comment_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('article_id');
+            $table->timestamps();
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
