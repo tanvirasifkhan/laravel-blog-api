2afd2e1eb9d400699b16f9987ab4b76aeb41f69b
Asif Khan <asifkhan.github@gmail.com>
2020-08-28 23:57:33 +0600

    article resource created

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'image'=>$this->image,
+            'title'=>$this->title,
+            'description'=>$this->body,
+            'category'=>$this->category->name,
+            'author'=>$this->author->name
+        ];
+    }
+}
