6e4bebd2847a64f76295d2567b1d442162e4241d
Asif Khan <asifkhan.github@gmail.com>
2020-08-29 00:06:15 +0600

    path bug fixed

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -7,12 +7,12 @@ use App\Models\User;
 use App\Models\Category;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Article::class, function (Faker $faker) {
     return [
         'title'=>$faker->sentence,
         'body'=>$faker->paragraph,
-        'author_id'=>App\Model\User::all(['id'])->random(),
-        'category_id'=>App\Model\Category::all(['id'])->random(),
+        'author_id'=>User::all(['id'])->random(),
+        'category_id'=>Category::all(['id'])->random(),
         'image'=>$faker->image(public_path('images'))
     ];
 });
