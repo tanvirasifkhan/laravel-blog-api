a9733ef751160881b2ac6838c4aa5b176b860d4f
Asif Khan <asifkhan.github@gmail.com>
2020-08-28 23:15:19 +0600

    category resource bug fixed

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -16,7 +16,7 @@ class CategoryResource extends JsonResource
     {
         return [
             'id'=>$this->id,
-            'name'=>$this->name,
+            'title'=>$this->title,
             'slug'=>$this->slug
         ];
     }
