0246685702b8fe168541e8f465688b7b8de9e14d
Asif Khan <asifkhan.github@gmail.com>
2020-08-28 23:49:49 +0600

    controller created for article

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+use Response;
+use App\Http\Resources\ArticleResource;
+use Illuminate\Validation\Rule;
+use App\Model\Article;
+use Auth;
+
+class ArticleController extends Controller {
+    
+}
