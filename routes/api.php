91aa506754c7b05fd512dd919eb2e5e423d6b8c8
Asif Khan <asifkhan.github@gmail.com>
2020-08-29 03:39:27 +0600

    middleware restriction added

--- routes/api.php
+++ routes/api.php
@@ -16,34 +16,34 @@ use Illuminate\Support\Facades\Route;
 
 /**********************************   Category Route Starts Here   *******************************************/
 Route::get('categories','CategoryController@index');
-Route::post('category/store','CategoryController@store');
+Route::post('category/store','CategoryController@store')->middleware('auth:api');;
 Route::get('category/{id}/show','CategoryController@show');
-Route::post('category/{id}/update','CategoryController@update');
-Route::post('category/{id}/remove','CategoryController@remove');
+Route::post('category/{id}/update','CategoryController@update')->middleware('auth:api');;
+Route::post('category/{id}/remove','CategoryController@remove')->middleware('auth:api');;
 Route::get('category/{keyword}/search','CategoryController@searchCategory');
 /**********************************   Category Route Ends Here   *******************************************/
 
 /**********************************   Article Route Starts Here   *******************************************/
 Route::get('articles','ArticleController@index');
-Route::post('article/store','ArticleController@store');
+Route::post('article/store','ArticleController@store')->middleware('auth:api');;
 Route::get('article/{id}/show','ArticleController@show');
-Route::post('article/{id}/update','ArticleController@update');
-Route::post('article/{id}/remove','ArticleController@remove');
+Route::post('article/{id}/update','ArticleController@update')->middleware('auth:api');;
+Route::post('article/{id}/remove','ArticleController@remove')->middleware('auth:api');;
 Route::get('article/{keyword}/search','ArticleController@searchArticle');
 Route::get('article/{id}/comments','ArticleController@comments');
 /**********************************   Article Route Ends Here   *******************************************/
 
 /**********************************   Comment Route Starts Here   *******************************************/
 Route::get('comments','CommentController@index');
-Route::post('comment/store','CommentController@store');
+Route::post('comment/store','CommentController@store')->middleware('auth:api');;
 Route::get('comment/{id}/show','CommentController@show');
-Route::post('comment/{id}/update','CommentController@update');
-Route::post('comment/{id}/remove','CommentController@remove');
+Route::post('comment/{id}/update','CommentController@update')->middleware('auth:api');;
+Route::post('comment/{id}/remove','CommentController@remove')->middleware('auth:api');;
 /**********************************   Comment Route Ends Here   *******************************************/
 
 /**********************************   Author Route Starts Here   *******************************************/
-Route::get('authors','AuthorController@index');
+Route::get('authors','AuthorController@index')->middleware('auth:api');;
 Route::post('register','AuthorController@register');
 Route::post('login','AuthorController@login');
-Route::post('logout','AuthorController@logout');
+Route::post('logout','AuthorController@logout')->middleware('auth:api');;
 /**********************************   Author Route Ends Here   *******************************************/
