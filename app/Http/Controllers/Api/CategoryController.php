0dc02be8c694b455ff192184597e7895b0e22953
Asif Khan <asifkhan.github@gmail.com>
2020-08-28 23:12:17 +0600

    method created for showing all the categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
+use Response;
+use App\Resources\CategoryResource;
+use Illuminate\Validation\Rule;
 
 class CategoryController extends Controller {
-    
+    // show all the categories
+    public function index(){
+        return CategoryResource::collection(Category::orderBy('id','DESC')->paginate(10));
+    }
 }
