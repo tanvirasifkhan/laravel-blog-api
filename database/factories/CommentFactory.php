9c29956f3da9c67fdc61e5f479df559eb1b65167
Asif Khan <asifkhan.github@gmail.com>
2020-08-29 02:49:25 +0600

    comment model path bug fixed

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,9 +4,10 @@
 
 use App\Models\User;
 use App\Models\Article;
+use App\Models\Comment;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Comment::class, function (Faker $faker) {
     return [
         'comment'=>$faker->sentence,
         'author_id'=>User::all(['id'])->random(),
