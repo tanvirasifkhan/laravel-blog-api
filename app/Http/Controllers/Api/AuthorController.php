173cd56a3b7ce1eef964f46da1531fedc9457483
Asif Khan <asifkhan.github@gmail.com>
2021-06-29 22:11:31 +0600

    modified

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -57,7 +57,7 @@ class AuthorController extends Controller {
             $author->password=bcrypt($request->password);
             $author->api_token=Str::random(80);
             $author->save();
-            return Response::json(['success'=>'Registration done successfully !','author'=>$author,'token'=>$author->api_token]);
+            return Response::json(['success'=>'Registration done successfully !','author'=>$author]);
         }
     }
 
@@ -74,9 +74,9 @@ class AuthorController extends Controller {
                 $author=$request->user();
                 $author->api_token=Str::random(80);
                 $author->save();
-                return Response::json(['success'=>'Login was successfully !','author'=>Auth::user(),'token'=>Auth::user()->api_token]);
+                return Response::json(['loggedin'=>true,'success'=>'Login was successfully !','author'=>Auth::user()]);
             }else{
-                return Response::json(['errors'=>'Login failed ! Wrong credentials.']);
+                return Response::json(['loggedin'=>false,'errors'=>'Login failed ! Wrong credentials.']);
             }
         }
     }
