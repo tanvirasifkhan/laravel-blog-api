68cba0abb86859e41254488415f7388ad37824ca
Asif Khan <asifkhan.github@gmail.com>
2021-07-06 01:03:56 +0600

    route created for checking article validation

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,7 @@ Route::get('article/{id}/comments','ArticleController@comments');
 /**********************************   Comment Route Starts Here   *******************************************/
 Route::get('comments','CommentController@index')->middleware('auth:api');
 Route::post('comment/check/comment','CommentController@checkComment')->middleware('auth:api');
+Route::post('comment/check/article','CommentController@checkArticle')->middleware('auth:api');
 Route::post('comment/store','CommentController@store')->middleware('auth:api');
 Route::get('comment/{id}/show','CommentController@show');
 Route::post('comment/{id}/update','CommentController@update')->middleware('auth:api');
